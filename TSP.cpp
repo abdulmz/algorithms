4cac1bf58e20a6f48aa10c67896ca0f1e844a44c
abdulmz <58999629+abdulmz@users.noreply.github.com>
2021-08-28 21:31:01 +0530

    Add files via upload

---
+++ TSP.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int tsp(const vector<vector<int>>& cities, int pos, int visited, vector<vector<int>>& state)
+{
+    if(visited == ((1 << cities.size()) - 1))
+        return cities[pos][0]; 
+
+    if(state[pos][visited] != INT_MAX)
+        return state[pos][visited];
+
+    for(int i = 0; i < cities.size(); ++i)
+    {
+        if(i == pos || (visited & (1 << i)))
+            continue;
+
+        int distance = cities[pos][i] + tsp(cities, i, visited | (1 << i), state);
+        if(distance < state[pos][visited])
+            state[pos][visited] = distance;
+    }
+
+    return state[pos][visited];
+}
+
+int main()
+{
+    vector<vector<int>> cities = { { 0, 10, 15, 20 },
+                       { 10, 0, 35, 25 },
+                       { 15, 35, 0, 30 },
+                       { 20, 25, 30, 0 } };
+
+    vector<vector<int>> state(cities.size());
+    for(auto& neighbors : state)
+        neighbors = vector<int>((1 << cities.size()) - 1, INT_MAX);
+
+    cout << "minimum: " << tsp(cities, 0, 1, state) << endl;
+
+    return 0;
+}
+
